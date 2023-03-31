b7632572c34df2ae4469e77fff14128ea7e6ce53
Sen <nevyn202@outlook.com>
2023-03-31 13:13:49 -0500

    Revert "font slider"
    This reverts commit ec7721de2cb08027b719fe852bf11afbb38cf45a.

--- frontend/src/assets/components/Connect.jsx
+++ frontend/src/assets/components/Connect.jsx
@@ -22,9 +22,9 @@ const Connect = () => {
     return (
         <>
         {connectionStatus ? (
-        <p className='connected'><b>Connected: {connectionStatus}</b></p>
+        <p className='connected'>Connected: {connectionStatus}</p>
         ) : (
-        <p className='disconnected'><b>Disconnected</b></p>
+        <p className='disconnected'>Disconnected</p>
         )}
     </>
     )
