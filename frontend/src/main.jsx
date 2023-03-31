1fb1ca44cd4294ec8b9fd7ef19cef792f1d00826
Sen <nevyn202@outlook.com>
2023-03-31 11:50:00 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ColorLoader from './assets/components/ColorLoader'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ColorLoader/>
   </React.StrictMode>,
 )
