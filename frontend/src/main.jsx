3e5f32fbf1aac8ddd7682c145a646593c46f68d9
ausboss <102309304+ausboss@users.noreply.github.com>
2023-04-04 20:21:48 -0500

    Tailwind.css classes working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import ColorLoader from './assets/components/ColorLoader'
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
