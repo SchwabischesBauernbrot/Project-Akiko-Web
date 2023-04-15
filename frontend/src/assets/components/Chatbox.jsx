f51662901effcf05f7066592f92f641e528207a8
Sen <nevyn202@outlook.com>
2023-04-15 10:03:10 -0500

    minor chatbox css changes

--- frontend/src/assets/components/Chatbox.jsx
+++ frontend/src/assets/components/Chatbox.jsx
@@ -524,10 +524,13 @@ function Chatbox({ endpoint, endpointType }) {
                 <Connect/>
               </div>
               <div className="ml-1">
-              <h4>Enable message shuffling</h4>
+              
+              <div className="flex flex-col items-center">
+              <h4>Enable Message Reordering</h4>
               <div className="flex items-center ml-1">
-            <input type='checkbox'className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" name='branchtoggle' checked={toggleBranch} onChange={() => handleCheckToggle()}/>
-          </div>
+              <input type='checkbox'className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" name='branchtoggle' checked={toggleBranch} onChange={() => handleCheckToggle()}/>
+              </div>
+              </div>
           </div>
               <div className="title-wrapper"> 
                 {conversation && (
