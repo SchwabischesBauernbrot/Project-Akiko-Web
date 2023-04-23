b6c1a07fb8e8b89d1cffd3998ae966a4e4e5d089
thesentinel2615 <nevyn202@outlook.com>
2023-04-22 19:52:25 -0500

    JS-ify

--- frontend/server.js
+++ frontend/server.js
@@ -22,7 +22,7 @@ const CHARACTER_IMAGES_FOLDER = './src/shared_data/character_images/';
 const CHARACTER_ADVANCED_FOLDER = './src/shared_data/advanced_characters/';
 const AUDIO_LOCATION = './src/audio/';
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
-
+const CONVERSATIONS_FOLDER = './src/shared_data/conversations/';
 function allowed_file(filename) {
     const allowed_extensions = ['png', 'jpg', 'jpeg', 'gif'];
     const extension = path.extname(filename).slice(1).toLowerCase();
@@ -254,4 +254,187 @@ app.post('/tts/generate/:voice_id', async (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+/*
+############################################
+##                                        ##
+##          Conversation ROUTES           ##
+##                                        ##
+############################################
+*/
+
+app.post('/conversation', (req, res) => {
+  const conversationData = req.body;
+  const chatName = conversationData.conversationName;
+
+  if (!fs.existsSync(CONVERSATIONS_FOLDER)) {
+      fs.mkdirSync(CONVERSATIONS_FOLDER);
+  }
+  const filePath = path.join(CONVERSATIONS_FOLDER, `${chatName}.json`);
+  try {
+      fs.writeFileSync(filePath, JSON.stringify(conversationData));
+      res.status(200).json({ status: 'success' });
+  } catch (e) {
+      console.error(`Error saving conversation: ${e.toString()}`);
+      res.status(500).json({ status: 'error', message: 'An error occurred while saving the conversation.' });
+  }
+});
+
+app.get('/conversations', (req, res) => {
+  if (!fs.existsSync(CONVERSATIONS_FOLDER)) {
+      res.json({ conversations: [] });
+      return;
+  }
+  const conversationFiles = fs.readdirSync(CONVERSATIONS_FOLDER);
+  const conversationNames = conversationFiles.map(file => path.parse(file).name);
+  res.json({ conversations: conversationNames });
+});
+
+app.route('/conversation/:conversation_name')
+  .get((req, res) => {
+      const convoPath = path.join(CONVERSATIONS_FOLDER, `${req.params.conversation_name}.json`);
+      try {
+          const convoData = JSON.parse(fs.readFileSync(convoPath));
+          res.json(convoData);
+      } catch (e) {
+          if (e.code === 'ENOENT') {
+              res.status(404).json({ error: 'Conversation not found' });
+          } else {
+              res.status(500).json({ error: 'An error occurred while reading the conversation.' });
+          }
+      }
+  })
+  .delete((req, res) => {
+      const convoPath = path.join(CONVERSATIONS_FOLDER, `${req.params.conversation_name}.json`);
+      try {
+          fs.unlinkSync(convoPath);
+          res.json({ message: 'Conversation deleted successfully' });
+      } catch (e) {
+          if (e.code === 'ENOENT') {
+              res.status(404).json({ error: 'Conversation not found' });
+          } else {
+              res.status(500).json({ error: 'An error occurred while deleting the conversation.' });
+          }
+      }
+  });
+
+/*
+############################################
+##                                        ##
+##        Advanced Character ROUTES       ##
+##                                        ##
+############################################
+*/
+
+app.delete('/advanced-character/:char_id/:emotion', (req, res) => {
+    const { char_id, emotion } = req.params;
+    const emotion_path = path.join(CHARACTER_ADVANCED_FOLDER, char_id, `${emotion}.png`);
+    const default_path = path.join(CHARACTER_ADVANCED_FOLDER, char_id, 'default.png');
+
+    if (fs.existsSync(emotion_path)) {
+        fs.unlinkSync(emotion_path);
+        if (emotion === 'default' && fs.existsSync(default_path)) {
+            fs.unlinkSync(default_path);
+        }
+        res.json({ success: `Character emotion ${emotion} deleted.` });
+    } else {
+        res.status(404).json({ failure: `Character does not have an image for the ${emotion} emotion.` });
+    }
+});
+
+app.get('/advanced-character/:char_id/:emotion', (req, res) => {
+    const { char_id, emotion } = req.params;
+    const emotion_path = path.join(CHARACTER_ADVANCED_FOLDER, char_id, `${emotion}.png`);
+    const default_path = path.join(CHARACTER_ADVANCED_FOLDER, char_id, 'default.png');
+
+    if (fs.existsSync(emotion_path)) {
+        const imagePath = path.join(CHARACTER_ADVANCED_FOLDER, char_id, `${emotion}.png`);
+        res.json({ success: 'Character emotion found', path: imagePath });
+    } else if (fs.existsSync(default_path)) {
+        const imagePath = path.join(CHARACTER_ADVANCED_FOLDER, char_id, 'default.png');
+        res.json({ failure: 'Character emotion not found, reverting to default', path: imagePath });
+    } else {
+        res.status(404).json({ failure: 'Character does not have an image for this emotion.' });
+    }
+});
+
+app.post('/advanced-character/:char_id/:emotion', (req, res) => {
+    const { char_id, emotion } = req.params;
+    const char_folder = path.join(CHARACTER_ADVANCED_FOLDER, char_id);
+    if (!fs.existsSync(char_folder)) {
+        fs.mkdirSync(char_folder);
+    }
+    const emotion_file = req.files?.emotion;
+    if (!emotion_file) {
+        res.status(500).json({ error: 'No emotion image file found' });
+        return;
+    }
+    const emotion_file_name = `${emotion}.png`;
+    emotion_file.mv(path.join(char_folder, emotion_file_name), err => {
+        if (err) {
+            res.status(500).json({ error: 'An error occurred while saving the emotion image file.' });
+        } else {
+            const imagePath = path.join(CHARACTER_ADVANCED_FOLDER, char_id, emotion_file_name);
+            res.json({ path: imagePath });
+        }
+    });
+});
+
+app.get('/advanced-character/:char_id', (req, res) => {
+    const char_folder = path.join(CHARACTER_ADVANCED_FOLDER, req.params.char_id);
+    if (fs.existsSync(char_folder)) {
+        const emotions_with_ext = fs.readdirSync(char_folder);
+        const emotions = emotions_with_ext.map(emotion => path.parse(emotion).name);
+        res.json({ success: 'Character emotions found', emotions });
+    } else {
+        res.status(404).json({ failure: 'Character does not have any emotions.' });
+    }
+});
+
+app.post('/character-speech/:char_id', (req, res) => {
+  const { char_id } = req.params;
+  const character_speech = req.body;
+  if (!character_speech) {
+      res.status(500).json({ error: 'No character speech data found' });
+      return;
+  }
+  const char_folder = path.join(CHARACTER_ADVANCED_FOLDER, char_id);
+  if (!fs.existsSync(char_folder)) {
+      fs.mkdirSync(char_folder);
+  }
+  fs.writeFile(path.join(char_folder, 'character_speech.json'), JSON.stringify(character_speech), err => {
+      if (err) {
+          res.status(500).json({ error: 'An error occurred while saving the character speech data.' });
+      } else {
+          res.send('Character speech saved successfully!');
+      }
+  });
+});
+/*
+############################################
+##                                        ##
+##             Speech ROUTES              ##
+##                                        ##
+############################################
+*/
+app.get('/character-speech/:char_id', (req, res) => {
+  const { char_id } = req.params;
+  const char_folder = path.join(CHARACTER_ADVANCED_FOLDER, char_id);
+  if (!fs.existsSync(char_folder)) {
+      res.status(404).json({ error: 'Character folder not found' });
+      return;
+  }
+  const speech_file = path.join(char_folder, 'character_speech.json');
+  if (!fs.existsSync(speech_file)) {
+      res.status(404).json({ error: 'Speech file not found' });
+      return;
+  }
+  fs.readFile(speech_file, (err, data) => {
+      if (err) {
+          res.status(500).json({ error: 'An error occurred while reading the character speech data.' });
+      } else {
+          res.json(JSON.parse(data));
+      }
+  });
 });
